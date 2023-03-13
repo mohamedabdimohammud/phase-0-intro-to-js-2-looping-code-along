1d8756c09b8ec9910a355a221b1ee534071913ee
mohamedabdimohammud <mmohammud@outlook.com>
2023-03-13 11:00:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// index.js
+
+// writeCards function implementation
+function writeCards(namesArray, event) {
+    const messagesArray = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      messagesArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return messagesArray;
+  }
+  
+  // countDown function implementation
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }  
